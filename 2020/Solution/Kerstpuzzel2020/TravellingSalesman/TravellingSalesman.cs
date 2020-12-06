dc51f08ffbcb6440e693de1ac5bbfade61e0f503
Egbert Botterman <egbert-jan.botterman@king.eu>
2020-12-06 19:52:38 +0100

    Clean table.

--- 2020/Solution/Kerstpuzzel2020/TravellingSalesman/TravellingSalesman.cs
+++ 2020/Solution/Kerstpuzzel2020/TravellingSalesman/TravellingSalesman.cs
@@ -4,25 +4,6 @@ using System.Text;
 
 namespace TravellingSalesman
 {
-    /*
-        "Alabama",890,614
-        "Texas",577,614
-        "Nebraska",571,323
-        "Arkansas",735,532
-        "SouthCarolina",1036,532
-        "RhodeIsland",1194,252
-        "Ohio",951,342
-        "Florida",1036,714
-        "Georgia",972,575
-        "Nevada",149,349
-        "Idaho",1069,391
-        "Tennesse",681,159
-        "Oregon",128,212
-        "Virginia",1068,390
-        "Minnesota",681,159
-        "Arizona",262,513
-        "Illinois",819,387
-     */
     public class TravellingSalesman
     {
         public List<State> _stippen = new List<State>();
